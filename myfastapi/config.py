9cbf2b0874377a7ed25bdefb19b12d2e7b539041
Harshini646 <harshini@silofortune.com>
2025-03-03 13:14:44 +0530

    Delete myfastapi directory

--- myfastapi/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Settings:
-    database_url: str = os.getenv(
-        'DATABASE_URL',
-        'mysql+pymysql://admin:NtxZCgSHUHwBAik5r5Ob@silo-dev-db.c0wteib2ljjz.ap-south-1.rds.amazonaws.com:3306/ecommerce'
-    )
-
-settings = Settings()
--- myfastapi/exceptions.py
+++
@@ -1,5 +0,0 @@
-class CustomException(Exception):
-    def __init__(self, name, message):
-        self.name = name
-        self.message = message
-        super().__init__(self.message)
